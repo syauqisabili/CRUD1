2ef15705b2023ae15a43160e36a49e5ff54d0f22
syauqisabili <syauqisabili@gmail.com>
2020-07-04 16:59:58 +0700

    4th commit

--- app/Models/PertanyaanModel.php
+++ app/Models/PertanyaanModel.php
@@ -13,7 +13,7 @@ class PertanyaanModel {
     //array associative [key][value]
     public static function insertData($data){
         unset($data["_token"]);
-        $data['created_at'] = Carbon::now('Y-m-d');
+        $data['created_at'] = Carbon::now(); //->toDateString() Equivalent: ->format('Y-m-d');
         $new_item = DB::table('pertanyaan')->insert($data);
         return $new_item;
     }
